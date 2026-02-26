4439ed94a9275659e8509cdf93beb9b301facf6a
Janik Stucki <Janik_Stucki@sluz.ch>
2026-02-26 12:33:05 +0100

    Started with the Homepage and navigation bar

---
+++ Frontend/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<template>
+<div class="footer">
+    <div>About | Legal | Contact</div>
+    <div>© 2026 BiteSite</div>
+</div>
+</template>
+
+<style>
+.footer{
+    font-family: var(--font);
+    color: var(--font-color-dark-blue);
+    font-size: 15px;
+    font-weight: bold;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-content: end;
+    align-content: flex-end;
+}
+</style>
---
+++ Frontend/src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+<div class="top">
+    <h1 class="title">BiteSite</h1>
+    <div class="navbar-items">
+        <h2>Restaurants</h2>
+        <h2>What BiteSite offers</h2>
+        <h2>Login</h2>
+    </div>
+</div>
+</template>
+
+<style>
+.title{
+    color: var(--accent);
+    cursor: pointer;
+}
+.top{
+    padding-left: 50px;
+    display: flex;
+    gap: 30px;
+    align-items: baseline;
+    font-family: var(--font);
+    cursor: pointer;
+}
+
+.navbar-items{
+    display: flex;
+    gap: 30px;
+    color: var(--font-color-dark-blue);
+    cursor: pointer;
+    font-size: 13px;
+}
+</style>
