956af0114709b1955853ac32ae2043247835b1e0
Janik Stucki <Janik_Stucki@sluz.ch>
2026-02-26 12:33:05 +0100

    feat(home, nav): implement homepage structure and create nav bar/footer

---
+++ Frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
+import Footer from './components/footer.vue'
+</script>
+
+<template>
+<div class="site-wrapper">
+    <NavBar/>
+    <div class="router-wrapper">
+        <RouterView/>
+    </div>
+    <Footer/>
+</div>
+</template>
+
+<style scoped>
+.site-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+}
+
+.router-wrapper {
+    flex: 1;
+}
+</style>
---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,79 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main-content">
+    <div class="description">
+      <h1>Showcase Your</h1>
+      <h1>Restaurant Online in</h1>
+      <h1>Minutes</h1>
+    </div>
+  <div class="buttons">
+    <button class="button">Get Started</button>
+    <button class="button"><div class="top-text">Find Restaurants</div><div class="bottom-text">Hosted by us</div></button>
+  </div>
+  <div class="right-bar"></div>
+</div>
+
+
+
+</template>
+
+<style>
+body {
+  background: var(--background);
+}
+
+.main-content{
+  padding-left: 50px;
+}
+.description{
+  padding-top: 50px;
+    font-family: var(--font);
+    color: var(--font-color-dark-blue);
+    line-height: 15px;
+}
+
+.button{
+  background-color: var(--accent);
+  border-radius: 90px;
+  height: 70px;
+  width: 210px;
+  border:none;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+  font-weight: 700;
+}
+
+.top-text{
+  font-size: 18px;
+}
+
+.bottom-text{
+  font-size: 14px;
+  font-weight: 300;
+}
+
+.buttons{
+  padding-top: 50px;
+  padding-left: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.buttons :hover{
+  background-color: var(--button-hover-color);
+}
+
+.right-bar {
+  position: fixed;
+  width: 300px;
+  height: 100vh;
+  right: 0px;
+  top: 0px;
+  background-color: var(--accent);
+}
+</style>
