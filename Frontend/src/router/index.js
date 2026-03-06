de2c85560821f9d7a303faa6ce9d57effdb7371c
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-03-06 11:13:25 +0100

    feat(user-settings): add user settings view route to index.js

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import DashboardView from '../views/DashboardView.vue'
+import UserSettingsView from '../views/UserSettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
+    },
+    {
+      path: '/user-settings',
+      name: 'user-settings',
+      component: UserSettingsView,
     },
   ],
 })
