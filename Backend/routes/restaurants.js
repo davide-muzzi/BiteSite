ce71b367b3f0d7f9198f5bd276427f481e923b15
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-03-25 13:28:14 +0100

    feat(newsletter): add newsletter endpoint

--- Backend/routes/restaurants.js
+++ Backend/routes/restaurants.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { routeWrapper } from "../error-handling.js";
 import checkAuth from "../middleware/check-auth.js";
+import * as restaurantHandlers from "../handlers/restaurants.js";
 
 const restaurantRouter = express.Router();
 
-
+restaurantRouter.post("/newsletter", checkAuth, routeWrapper(restaurantHandlers.sendNewsletter));
 
 export default restaurantRouter
