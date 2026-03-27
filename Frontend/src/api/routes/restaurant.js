9a288e31279b55f4ecbdb3b31a18dd651b534892
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-03-27 14:55:41 +0100

    feat(newsletter): implement attachements to sendNewsletter request function

--- Frontend/src/api/routes/restaurant.js
+++ Frontend/src/api/routes/restaurant.js
@@ -1,6 +1,5 @@
 import request from '../request.js'
 
-export async function sendNewsletter(projectId, subject, body) {
-  // Todo: implement attachements
-  return request('post', '/restaurants/send-newsletter', { data: { projectId, subject, body } })
+export async function sendNewsletter(projectId, subject, body, attachments = []) {
+  return request('post', '/restaurants/send-newsletter', { data: { projectId, subject, body, attachments } })
 }
