efa2cead006a5ac6c145fe41b5d34904869ed833
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-03-27 16:16:17 +0100

    fix(newsletter): fix newsletter api route

--- Frontend/src/api/routes/restaurant.js
+++ Frontend/src/api/routes/restaurant.js
@@ -1,5 +1,5 @@
 import request from '../request.js'
 
 export async function sendNewsletter(projectId, subject, body, attachments = []) {
-  return request('post', '/restaurants/send-newsletter', { data: { projectId, subject, body, attachments } })
+  return request('post', '/restaurants/newsletter', { data: { projectId, subject, body, attachments } })
 }
