653c20c57dcff713b2189a61a5e1104e08481287
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-04-02 14:24:33 +0200

    refactor: rename send newsletter route

--- Backend/routes/restaurants.js
+++ Backend/routes/restaurants.js
@@ -6,6 +6,6 @@ import * as restaurantHandlers from "../handlers/restaurants.js";
 const restaurantRouter = express.Router();
 
 restaurantRouter.post("/newsletter/subscribe", routeWrapper(restaurantHandlers.subscribeToNewsletter));
-restaurantRouter.post("/newsletter", checkAuth, routeWrapper(restaurantHandlers.sendNewsletter));
+restaurantRouter.post("/newsletter/send", checkAuth, routeWrapper(restaurantHandlers.sendNewsletter));
 
 export default restaurantRouter
